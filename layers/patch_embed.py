4a16eab44609c6800f23d1bef328ee0779c59eba
Shilu <shilu842003@gmail.com>
2023-06-20 09:16:57 +0530

    added patch_embed layer

---
+++ layers/patch_embed.py
@@ -0,0 +1,46 @@
+from tensorflow import keras 
+import tensorflow as tf 
+from tensorflow.keras import Model 
+from tensorflow.keras.layers import *
+
+
+
+class PatchEmbed(keras.layers.Layer):
+    """ Image to Patch Embedding
+    Args:
+        img_size (int): Image size.  Default: 224.
+        patch_size (int): Patch token size. Default: 4.
+        in_chans (int): Number of input image channels. Default: 3.
+        embed_dim (int): Number of linear projection output channels. Default: 96.
+        norm_layer (nn.Module, optional): Normalization layer. Default: None
+    """
+    def __init__(self, img_size=(224, 224), patch_size=(4, 4), in_chans=3, embed_dim=96, norm_layer=None):
+        super().__init__()
+        self.img_size = (img_size)
+        self.patch_size = (patch_size)
+        patches_resolution = [img_size[0] // patch_size[0], img_size[1] // patch_size[1]]
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.patches_resolution = patches_resolution
+        self.num_patches = patches_resolution[0] * patches_resolution[1]
+
+        self.in_chans = in_chans
+        self.embed_dim = embed_dim
+
+        self.proj = Conv2D(filters=embed_dim,
+                           kernel_size=patch_size,
+                           strides=patch_size
+                          )
+        if norm_layer is not None:
+            self.norm = norm_layer(epsilon=1e-5, name='norm')
+        else:
+            self.norm = None
+            
+    def call(self, x):
+        B, H, W, C = tf.shape(x)[0], tf.shape(x)[1], tf.shape(x)[2], tf.shape(x)[3]
+      
+        x = self.proj(x)
+        x = tf.reshape(x, shape=[-1, (H // self.patch_size[0]) * (W // self.patch_size[0]), self.embed_dim])
+        if self.norm is not None:
+            x = self.norm(x)
+        return x  
